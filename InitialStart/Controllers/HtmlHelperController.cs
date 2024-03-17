5cdc5c5c2bd626d203210529449497754d00299f
Divarci <hasandivarciuk@gmail.com>
2024-03-17 23:23:17 +0000

    Html Helpers

---
+++ InitialStart/Controllers/HtmlHelperController.cs
@@ -0,0 +1,23 @@
+﻿using InitialStart.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InitialStart.Controllers
+{
+    public class HtmlHelperController : Controller
+    {
+        public IActionResult Helper()
+        {
+            ViewBag.JustNumber = 55;
+
+            Product product = new Product()
+            {
+                Id = 1,
+                Title = "Test",
+                StockLevel = 1,
+                StockStatus = true
+            };
+
+            return View(product);
+        }
+    }
+}

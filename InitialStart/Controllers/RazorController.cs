99d36959b175299eb6da14896a58e67ee82f286b
Divarci <hasandivarciuk@gmail.com>
2024-03-17 20:04:19 +0000

    Razor Lessons

---
+++ InitialStart/Controllers/RazorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InitialStart.Controllers
+{
+    public class RazorController : Controller
+    {
+        public IActionResult RazorLessons()
+        {
+            return View();
+        }
+    }
+}

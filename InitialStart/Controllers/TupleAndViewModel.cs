bfa954b6fc95ea9aa5178df03b48c7e1f76ecac4
Divarci <hasandivarciuk@gmail.com>
2024-03-17 19:28:28 +0000

    Tuple and ViewModel

---
+++ InitialStart/Controllers/TupleAndViewModel.cs
@@ -0,0 +1,47 @@
+﻿using InitialStart.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InitialStart.Controllers
+{
+    public class TupleAndViewModel : Controller
+    {
+        public IActionResult Test()
+        {
+            Product product = new()
+            {
+                Id = 1,
+                StockLevel = 15,
+                StockStatus = true,
+                Title="Fryer"
+            };
+
+            Category category = new()
+            {
+                Id = 1,
+                Name = "Electronics"
+            };
+
+            #region MultiDataTransfer
+            #endregion
+
+            #region UsingViewModel
+            //ProductCategory productCategory = new()
+            //{
+            //    Category = category,
+            //    Product = product
+            //};
+            //return View(productCategory);
+            #endregion
+            #region UsingTuple
+
+            var productCategory = (product, category);
+
+
+            return View(productCategory);
+            #endregion
+
+
+
+        }
+    }
+}
---
+++ InitialStart/Models/ViewModels/ProductCategory.cs
@@ -0,0 +1,10 @@
+﻿using InitialStart.Models.Entities;
+
+namespace InitialStart.Models.ViewModels
+{
+    public class ProductCategory
+    {
+        public Product Product { get; set; }
+        public Category Category { get; set; }
+    }
+}

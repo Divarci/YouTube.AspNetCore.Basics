153aefc4d3fd8c6ed676e2e7a286e1353ad97b7d
Divarci <hasandivarciuk@gmail.com>
2024-03-24 17:29:57 +0000

    Custom Tag Helpers

---
+++ InitialStart/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace InitialStart.TagHelpers
+{
+    //[HtmlTargetElement("attribute")]
+    public class EmailTagHelper : TagHelper
+    {
+        public string Email { get; set; }
+        public string Display { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.Add("href", $"mailto:{Email}");
+            output.Content.Append(Display);
+        }
+    }
+}

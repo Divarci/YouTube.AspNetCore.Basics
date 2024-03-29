2117e2aea735ebeab6b62c76edeb449d83d023f9
Divarci <hasandivarciuk@gmail.com>
2024-03-29 11:11:12 +0000

    Post and Catch Tuples

---
+++ InitialStart/Controllers/PostAndCatchTuplesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InitialStart.Controllers
+{
+    public class PostAndCatchTuplesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {   
+            var tuple = (new TestObjectOne(),new TestObjectTwo());
+            return View(tuple);
+        }
+
+        [HttpPost]
+        public IActionResult Create([Bind(Prefix ="Item1")]TestObjectOne objectOne, [Bind(Prefix = "Item2")] TestObjectTwo objectTwo)
+        {
+            return View();
+        }
+    }
+
+    public class TestObjectOne
+    {
+        public string Name { get; set; }
+    }
+
+    public class TestObjectTwo
+    {
+        public string Address { get; set; }
+    }
+}

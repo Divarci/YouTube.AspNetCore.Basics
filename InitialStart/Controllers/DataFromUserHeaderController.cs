97f4b4f970b98c4e440bbb3f422384867fa6f3cc
Divarci <hasandivarciuk@gmail.com>
2024-03-29 10:20:53 +0000

    Data From User Headers

---
+++ InitialStart/Controllers/DataFromUserHeaderController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InitialStart.Controllers
+{
+    public class DataFromUserHeaderController : Controller
+    {
+        #region Explanation
+        #endregion
+        //all requests have a header and body like an email. previous lessons we send and catch datas from body. Now we will see the HEADER.
+
+        //we use headers to send JWT tokens for authentication but we now i will show you how can we catch them 
+
+        #region Example
+        #endregion
+        
+        public IActionResult Index()
+        {
+            var request = HttpContext.Request.Headers.ToList();
+
+            var channelname = request.FirstOrDefault(x=>x.Key.Contains("Channel")).Value;
+
+            return View();
+        }
+    }
+}

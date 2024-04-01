cde3cbbb860c2f260c49c261ba69c389ff1f6bb2
Divarci <hasandivarciuk@gmail.com>
2024-04-01 14:00:34 +0100

    Validation

---
+++ InitialStart/Controllers/ValidationController.cs
@@ -0,0 +1,44 @@
+﻿using InitialStart.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InitialStart.Controllers
+{
+    public class ValidationController : Controller
+    {
+        #region CONTENTS
+        #endregion
+
+        //what is the validation?
+        //why should we use validation?
+        //what are the type of validations?
+        //how can we add our validation logic?
+        //how can we catch the those error?
+        //how can we show them to user?
+
+        #region EXAMPLE
+        #endregion
+
+        [HttpGet]
+        public ActionResult CreateCompany()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateCompany(Company request)
+        {
+            //modelsate
+
+            if(!ModelState.IsValid)
+            {
+                //log
+                //return a meaningfull message to user
+                //you can do everything you want here after validation
+                return View(request);
+            }
+
+            //we can do our algoritms or add-update to our database
+            return View();
+        }
+    }
+}
---
+++ InitialStart/Models/Entities/Company.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InitialStart.Models.Entities
+{
+    public class Company
+    {
+        [StringLength(25,ErrorMessage ="opps it is more than 25")]
+        [Required(ErrorMessage ="hey bro add a company name")]
+        public string CompanyName { get; set; }
+        [StringLength(50, ErrorMessage = "opps it is more than 50")]
+        [Required(ErrorMessage = "hey bro add an address")]
+        public string Address { get; set; }
+        public string? Phone { get; set; }
+        [EmailAddress(ErrorMessage ="please input valid email address")]
+        [Required(ErrorMessage = "I need an email")]
+        public string Email { get; set; }
+        public int? EmployeeCount { get; set; }
+    }
+}
+
+
+//company name must be lower than 25 character
+//company name cannot be null
+
+//address name must be lower than 50 character
+//adress name cannot be null
+
+//email can not be null and must be an email format
+

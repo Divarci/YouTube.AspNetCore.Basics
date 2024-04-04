bc0799fac1437ea13d6a1fce8838cd406d64d3f8
Divarci <hasandivarciuk@gmail.com>
2024-04-04 23:24:40 +0100

    View Components

---
+++ InitialStart/ViewComponents/AdvertisementViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InitialStart.ViewComponents
+{
+    public class AdvertisementViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            List<string> list = new() { "michael","amanda" };
+            return View(list);
+        }
+    }
+}

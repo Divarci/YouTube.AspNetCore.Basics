a3dc472d2e86896410ec94456c8bd4a53f0dfc57
Divarci <hasandivarciuk@gmail.com>
2024-04-16 22:30:05 +0100

    Api explanation part2

---
+++ ApiProject/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return BadRequest();
+        }
+    }
+}
